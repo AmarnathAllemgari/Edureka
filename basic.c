40202b986165cf6aee47074636361e778245fd7e
AmarnathAllemgari <88319240+AmarnathAllemgari@users.noreply.github.com>
2021-08-12 17:28:36 +0530

    Create basic.c
    This is a c program to convert decimal to hexa

---
+++ basic.c
@@ -0,0 +1,10 @@
+//This is very basic c program
+//This converts values from decimal to hexadecimal
+#include<stdio.h>
+void main()
+{
+  int x;
+  printf("Enter a decimal value:");
+  scanf("%d",x);
+  printf("%d in Hexadecimal value is %x",x,x);
+}
